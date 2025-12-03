5863f3d1cdc8c9675bdae93753981e967f5cf8e4
Alok Mishra <alok@alokmishra.com>
2025-12-02 19:11:47 -0500

    sync vscode neovim undo/redo with vscode's native undo stack
    Map u and Ctrl-r to VSCode's undo/redo commands to prevent buffer
    desync issues. Disable undofile creation in VSCode mode.

--- lua/core/vscode.lua
+++ lua/core/vscode.lua
@@ -5,6 +5,13 @@ vim.opt.smartcase = true
 vim.opt.ignorecase = true
 vim.opt.clipboard = "unnamedplus"
 
+-- Disable Neovim's undo file creation
+vim.opt.undofile = false
+
+-- Sync undo/redo with VSCode's native undo stack
+vim.keymap.set("n", "u", "<cmd>call VSCodeNotify('undo')<cr>", { silent = true })
+vim.keymap.set("n", "<C-r>", "<cmd>call VSCodeNotify('redo')<cr>", { silent = true })
+
 -- Keymaps
 vim.keymap.set("i", "jk", "<Esc>")
 vim.keymap.set("i", "kj", "<Esc>")
